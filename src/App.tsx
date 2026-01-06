2fbfcd8740b25a06164c1add2ca5e7e8043bed9a
Jolly Sharma <jollysharma@Jollys-MacBook-Air.local>
2026-01-06 15:50:52 +0530

    Fix Techstack import casing

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Services from "./components/Services";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import About from "./components/About";
-import TechStack from "./components/Techstack";
+import TechStack from "./components/TechStack";
 
 function App() {
   return (
